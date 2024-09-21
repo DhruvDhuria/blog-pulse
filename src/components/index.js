2598aed4ff225eec82eb6523e4ed96c4b3e7d45a
Dhruv Dhuria <dhuriadhruv13@gmail.com>
2024-09-21 14:17:21 +0530

    Configure Redux Toolkit and build components

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./Container/Container";
+import LogoutBtn from "./Header/LogoutBtn";
+import Logo from "./Logo";
+import Button from "./Button"
+import Input from "./Input"
+import Select from "./Select"
+import RTE from './RTE'
+import AuthLayout from './AuthLayout'
+import Signup from './Signup'
+import Login from './Login'
+import PostCard from './PostCard'
+import PostForm from './post-form/PostForm'
+
+
+export {Header, Footer, Container, LogoutBtn, Logo, Button, Input, Select, RTE, AuthLayout, Signup, Login, PostCard, PostForm}
